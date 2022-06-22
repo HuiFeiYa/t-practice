e13247c102c4fd3a74fdd06837e3b73101afd023
宋远桥 <wanxiao.guan@ideacome.com>
2022-06-22 10:06:15 +0800

    [feature] 初始化

--- home/global.d.ts
+++
@@ -1,4 +0,0 @@
-
-declare namespace console{
-  function log(s:string):void
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// src/index.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-var foo_1 = require("foo");
-console.log(foo_1.foo.name);
-foo_1.foo.bar.baz();
-//# sourceMappingURL=index.js.map
